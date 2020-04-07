815696cdd118d1ae6cc0e58c8fbefcc326772bd6
Dawid <alkuzad@gmail.com>
2020-04-07 22:13:01 +0200

    update

--- separate.py
+++ separate.py
@@ -1,32 +1,32 @@
 
 import sys
 import json
-from pathlib import Path
+from pathlib import WindowsPath
 
 import fire
 
 PREFIX = 'unxutils-'
 
 def binary_name(binary_path: str):
-    return Path(binary_path).stem
+    return WindowsPath(binary_path).stem
 
 def binary_to_package_name(binary_path: str):
     return f"{PREFIX}{binary_name(binary_path)}"
 
-def single_binary_content(source_json: dict, binary_name: str):
+def single_binary_content(source_json: dict, binary_path: str):
     new_json = source_json
-    new_json['bin'] = [binary_name]
+    new_json['bin'] = [binary_path, f"{WindowsPath(binary_path).parent}\\l{WindowsPath(binary_path).name}"]
     return new_json
 
-def separate(source: Path, target: Path = Path('bucket')):
-    source = Path(source)
+def separate(source: WindowsPath, target: WindowsPath = WindowsPath('bucket')):
+    source = WindowsPath(source)
     source_data = source.read_text()
     source_json = json.loads(source_data)
 
     for binary in source_json['bin']:
             if isinstance(binary, list):
                 continue
-            target_file = Path(target, f"{binary_to_package_name(binary)}.json")
+            target_file = WindowsPath(target, f"{binary_to_package_name(binary)}.json")
             target_content = single_binary_content(source_json, binary)
             target_file.write_text(json.dumps(target_content))
 
